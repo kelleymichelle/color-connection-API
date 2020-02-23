4cbb9c03759ae345b6e16f2201c5b8d49b963fea
Kelley Chaplain <kmchaplain@gmail.com>
2020-02-23 12:50:16 -0600

    added helper method to user account to validate password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :password, presence: true, if: :setting_password?
   validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
 
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  
+
+  def setting_password?
+    password || password_confirmation
+  end
+
 end
